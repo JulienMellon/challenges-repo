36bec181b1581acb8d7f19ab2ef7cf87e154986c
Julien Mellon <101375846+JulienMellon@users.noreply.github.com>
2022-04-05 07:11:02 -0400

    Create removeStringSpaces.js

---
+++ removeStringSpaces.js
@@ -0,0 +1,12 @@
+/*
+Description:
+Simple, remove the spaces from the string, then return the resultant string.
+*/
+
+//mySolution
+function noSpace(str){
+ return str.split(' ').join('')
+}
+
+//
+function noSpace(x){return x.split(' ').join('')}
