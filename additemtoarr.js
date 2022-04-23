4cc4570e7e8485aa47539fa2f31fadffa951f02b
Julien Mellon <101375846+JulienMellon@users.noreply.github.com>
2022-04-23 06:54:09 -0400

    Create additemtoarr.js

---
+++ additemtoarr.js
@@ -0,0 +1,9 @@
+// add the value "codewars" to the already defined websites array
+
+//given
+
+var websites = []
+
+// websites.push('codewars')
+// websites.unshift('codewars')
+websites.splice(0,0,'codewars')
