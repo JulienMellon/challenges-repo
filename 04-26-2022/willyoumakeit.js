953375b9b238cb3e81585e9143491f3695288c58
Julien Mellon <mellon.julien@gmail.com>
2022-04-26 21:47:32 -0400

    will you make it fuel calculation challenge complete

---
+++ 04-26-2022/willyoumakeit.js
@@ -0,0 +1,17 @@
+/*
+Will you make it?
+You were camping with your friends far away from home, but when it's time to go back, you realize that your fuel is running out and the nearest pump is 50 miles away! You know that on average, your car runs on about 25 miles per gallon. There are 2 gallons left. Considering these factors, write a function that tells you if it is possible to get to the pump or not. Function should return true (1 in Prolog, NASM and COBOL) if it is possible and false (0 in Prolog, NASM and COBOL) if not. The input values are always positive.
+*/
+
+/**
+ * 
+ * @param {how far am i from pump} distanceToPump 
+ * @param {miles per gallon} mpg 
+ * @param {how much fuel is left} fuelLeft 
+ * @returns {true if fuel left is enough to reach distanceToPump}
+ */
+
+const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
+    return fuelLeft >= distanceToPump / mpg
+    // TODO
+    };
