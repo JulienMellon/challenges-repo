a8c2a90987cfa92789adb59ac74428ec2e59b964
Julien Mellon <mellon.julien@gmail.com>
2022-04-27 07:04:15 -0400

    filter array and interpolation challenges

---
+++ 04-27-2022/vowelremover.js
@@ -0,0 +1,17 @@
+/* 
+Create a function called shortcut to remove the lowercase vowels (a, e, i, o, u ) in a given string.
+
+Examples
+"hello"     -->  "hll"
+"codewars"  -->  "cdwrs"
+"goodbye"   -->  "gdby"
+"HELLO"     -->  "HELLO"
+*/
+
+function shortcut (str) {
+    let split = str.split('')
+    let vowels = ['a', 'e', 'i', 'o', 'u']
+    let filtered = split.filter( (x) => !vowels.includes(x) )
+    let joined = filtered.join('')
+    return joined
+  }
