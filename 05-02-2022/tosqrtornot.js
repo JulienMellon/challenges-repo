27b3d193f06166252093b3141dd37e6aa5d070f8
Julien Mellon <mellon.julien@gmail.com>
2022-05-03 08:29:40 -0400

    added mergearrays.js

--- 05-02-2022/tosqrtornot.js
+++ 05-02-2022/tosqrtornot.js
@@ -4,15 +4,15 @@ Return a new array with processing every number of the input-array like this:
 
 If the number has an integer square root, take this, otherwise square the number.
 
-Example
+Example 
 [4,3,9,7,2,1] -> [2,9,3,49,4,1]
 Notes
-The input array will always contain only positive numbers, and will never be empty or null. */
+The input array will always contain only positive numbers, and will never be empty or null.  */
 
 
 
 function squareOrSquareRoot(array) {
-    let newarr = []
+    let newarr = [ ]
     array.forEach(x => {
       if ( Number.isInteger(Math.sqrt(x)) ){
         newarr.push(Math.sqrt(x))
