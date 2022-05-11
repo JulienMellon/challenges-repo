1653e338fee9bfaa073fdc752e39eefeea3ea883
Julien Mellon <mellon.julien@gmail.com>
2022-05-11 11:37:32 -0400

    another one down!

---
+++ definecardsuit.js
@@ -0,0 +1,9 @@
+function defineSuit (card) {
+    console.log('isworking')
+    // card.includes('♣')? 'clubs'
+    // : card.includes('♦')? 'diamonds'
+    // : card.includes('♥')? 'hearts'
+    // : card.includes('♠')? 'spades'
+}
+
+// console.log( defineSuit('3♣') )
