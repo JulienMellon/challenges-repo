f70869c44f8943e5aa535d4754058dae9f9f6279
Julien Mellon <mellon.julien@gmail.com>
2022-05-12 07:25:36 -0400

    an easy one

---
+++ function1-helloworld.js
@@ -0,0 +1,21 @@
+const greet = () => {
+    if ( Date() <= `The year 1877` ) {
+      return 'Ahoy World!'
+    }else{
+      return 'hell' + 'o' + ' ' + 'world'
+  }
+   
+// the origin of the world hello dates back to the 
+// first telephone call ever made, way back to the 1870s, when 
+// Americans would see an acquaintaince on the road and 
+
+// function greetFriend(){
+//   return 'Hoi! Hoi!'
+// }
+
+// 'ahoy' and 'hail' where also popular at the time.
+
+// Thomas Edison, the Elon Musk of his day, in his first phone call
+// wanted to be a little controversial, and decided to take the 
+// FOULEST word of the time... "hell" and he added a vowel to make it
+// ring... and thus hello was born
