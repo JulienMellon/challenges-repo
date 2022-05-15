fb5bdd5a71ff4bc7c04a45a5cd18ede8029204f7
Julien Mellon <mellon.julien@gmail.com>
2022-05-14 20:28:45 -0400

    working on it...

---
+++ harvesttime.js
@@ -0,0 +1,16 @@
+//PREP
+//P: seed: string, single character
+//  water: number of 'stem' characters
+//  water: determines how many time the stem/flower combo repeats
+//  fert: number of flower characters
+//  temp: number, integer, if between 20 and 30C; plant is normal, otherwise all flowers die except for one flower at end
+
+//R : string, with stems and flowers,  with length stems + flowers , and a flower at the end
+
+function plant(seed, water, fert, temp){
+ let str = ''
+ 
+}
+
+//plant(#, 2, 2, 25) => --##--##
+//plant(%, 4, 3, 10) => ---- ---- ---- %
