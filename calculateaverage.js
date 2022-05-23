9c09c04a6c89c8d3fc28dc807ffe767299575903
Julien Mellon <mellon.julien@gmail.com>
2022-05-23 06:52:08 -0400

    today is monday and i am pushing code today

---
+++ calculateaverage.js
@@ -0,0 +1,12 @@
+/* Write a function which calculates the average of the numbers in a given list.
+
+Note: Empty arrays should return 0.
+*/
+
+function find_average(array) {
+    let sum = 0
+    for ( let i = 0; i < array.length; i++ ){
+     sum += array[i]  
+    }
+     return array.length == 0 ? 0 : sum / array.length;
+   }
---
+++ pigit.js
@@ -0,0 +1,17 @@
+// PREP
+// Parameters : given a string, string has words and punctuation, words are separated by spaces, substrings do not end with punctuation
+//Return: a string, where first letter is now the last, and add 'ay'.  Not true Pig Latin; no different treatment for words starting with vowels.  punctuation will be in separate substrings, 
+
+//Examples : 
+pigIt('Pig latin is cool'); // igPay atinlay siay oolcay
+
+
+function pigIt(str){
+
+//take a string and...
+//split it up into an array of words divided by spaces
+// take each of those words and move the first letter to the end
+// add ay to wach of those words
+// glue it all back together
+
+}
