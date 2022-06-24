5a134008ba12df593f02ef56c8945799857c532a
Julien Mellon <mellon.julien@gmail.com>
2022-06-24 15:23:53 -0400

    added square.js

---
+++ square.js
@@ -0,0 +1,10 @@
+//Now you have to write a function that takes an argument and returns the square of it.
+
+function square(num){
+  return num**2
+}
+
+// lol there was an error and  i rewrote this so 
+
+// const square = num => num**2 
+//is what i submitted to codewars lol
